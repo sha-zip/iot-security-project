dbd2a19790c6008cab377b0abe4bd06dc1b23939
sha-zip <shahammami@gmail.com>
2026-02-14 13:50:16 +0000

    Describe what i changed

--- iot-agent/se_module.py
+++ iot-agent/se_module.py
@@ -1,16 +1,14 @@
 from pkcs11 import lib, Mechanism, ObjectClass
-
-PKCS11_MODULE= "/usr/lib/softhsm/libsofthsm2.so"
-TOKEN_LABEL= "iot_secure_element"
-KEY_LABEL= "iot_rsa_key"
+PKCS11_MODULE = "/usr/lib/softhsm/libsofthsm2.so"
+TOKEN_LABEL= "IoT_Secure_Element"
+KEY_LABEL="IoT_RSA_Key"
 USER_PIN= "0000"
 
-def sign_data(data: bytes) -> bytes:
-    pkcs11 = lib(PKCS11_MODULE)
-    
-    token = pkcs11.get_token(token_label=TOKEN_LABEL)
-    with token.open(user_pin=USER_PIN) as session:
-     private_key = session.get_key(label=KEY_LABEL, object_class=ObjectClass.PRIVATE_KEY)
-     signature = private_key.sign(data,mechanism=Mechanism.SHA256_RSA_PKCS)
-     return signature
+def sign_data(data: bytes) -> bytes :
+	pkcs11= lib(PKCS11_MODULE)
+	token= pkcs11.get_token(token_label=TOKEN_LABEL)
 
+	with token.open(user_pin=USER_PIN) as session:
+		private_key = session.get_key(label=KEY_LABEL, object_class=ObjectClass.PRIVATE_KEY)
+		signature= private_key.sign (data, mechanism=Mechanism.SHA256_RSA_PKCS)
+		return signature 
