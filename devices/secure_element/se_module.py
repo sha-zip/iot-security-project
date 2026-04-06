a2d80315ea2af671105d95df578c2c3bd04eaca7
sha-zip <shahammami@gmail.com>
2026-04-06 13:45:17 +0000

    modification se_module.py et ajout de requirements.txt

--- devices/secure_element/se_module.py
+++ devices/secure_element/se_module.py
@@ -3,13 +3,16 @@ import logging
 import pkcs11
 import pkcs11.exceptions
 from pkcs11 import lib, Mechanism, ObjectClass, KeyType
-
+from cryptography import x509
+from cryptography.hazmat.primitives import hashes, serialization 
+from cryptography.x509.oid import NameOID
 logging.basicConfig(level=logging.INFO)
 
 PKCS11_MODULE = os.environ.get("PKCS11_MODULE" , "/usr/lib/softhsm/libsofthsm2.so")
 TOKEN_LABEL = os.environ.get("SE_TOKEN_LABEL", "IoT_Secure_Element")
 KEY_LABEL = os.environ.get("SE_KEY_LABLE", "IoT_RSA_Key")
 USER_PIN = os.environ.get("SE_USER_PIN")
+DEVICE_ID= os.environ.get("DEVICE_ID", "iot-agent-01")
 
 def sign_data(data: bytes) -> bytes:
 	if not isinstance(data, bytes):
@@ -37,3 +40,11 @@ def sign_data(data: bytes) -> bytes:
 	except Exception as e :
 		logging.exception("Erreur inattendue dans sign_data")
 		raise RuntimeError("Erreur interne Secure Element") from e 
+
+def generate_csr(session, key_label: str) -> bytes:
+	private_key = session.get_key(lable=key_label,object_class=pkcs11.ObjectClass.PRIVATE_KEY )
+	if private_key is None:
+		raise RuntimeError(f"Cle '{key_label}' introuvable pour la CSR")
+	csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([x509.NameAttribute(NameOID.COMMON_NAME, DEVICE_ID), x509.NameAttribute(NameOID.ORGANIZATION_NAME, "IoT_Project"), x509.NameAttribute(NameOID.Country_Name, "TN"),])).sign(private_key, hashes.SHA256())
+	logging.info("CSR generee pour '%s'", DEVICE_ID)
+	return csr.public_bytes(serialization.Encoding.PEM)
