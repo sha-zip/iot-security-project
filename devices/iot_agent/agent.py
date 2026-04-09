5999a1f635afa09389ad0fb50f68b0b333844449
sha-zip <shahammami@gmail.com>
2026-04-09 11:13:43 +0000

    openssl.cnf

--- devices/iot_agent/agent.py
+++ devices/iot_agent/agent.py
@@ -9,7 +9,7 @@ import json
 # 👉 import depuis ton Secure Element
 from devices.secure_element.se_module import generate_csr  
 
-BACKEND_URL = os.environ.get("BACKEND_URL", "https://127.0.0.1:5000")
+BACKEND_URL = os.environ.get("BACKEND_URL", "http://127.0.0.1:5000")
 SECRET = os.environ["SECRET_KEY"].encode()
 DEVICE_ID = os.environ.get("DEVICE_ID", "iot-agent-01")
 
@@ -61,7 +61,7 @@ def send_csr_to_pki(csr):
     response = requests.post(
         BACKEND_URL + "/enroll",
         data=csr,
-        headers={"Content-Type": "application/x-pem-file"},
+        headers={"Content-Type": "application/x-pem-file","X-API-KEY": os.environ.get("API_KEY")},
         verify=False  # ⚠️ dev seulement
     )
 
