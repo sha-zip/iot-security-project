657ba61c8b7764dec43d72d7a8f4f4e5c6eae895
sha-zip <shahammami@gmail.com>
2026-04-19 20:01:54 +0100

    Add PKCS#11 token management functionality
    Initialize PKCS#11 library and manage tokens.

--- PY.py
+++ PY.py
@@ -1,2 +1,18 @@
-gggg
+import os, pkcs11
 
+libpath = os.getenv("PKCS11_LIB", "/usr/lib/softhsm/libsofthsm2.so")
+label = os.getenv("SE_TOKEN_LABEL", "iot-token")
+pin = os.getenv("SE_USER_PIN", "1234")
+
+lib = pkcs11.lib(libpath)
+tokens = list(lib.get_tokens(token_label=label))
+print("Tokens trouvés:", len(tokens))
+if not tokens:
+    raise SystemExit("Token introuvable")
+
+token = tokens[0]
+print("Token:", token)
+session = token.open(user_pin=pin, rw=True)
+print("Session ouverte OK")
+session.close()
+print("Session fermée OK")
