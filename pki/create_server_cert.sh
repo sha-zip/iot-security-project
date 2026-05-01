62b96f7a81884acf97d23f167d8900b77153260e
nadine-touil <nadine.std.touil@gmail.com>
2026-05-01 23:41:20 +0000

    update docker

--- pki/create_server_cert.sh
+++ pki/create_server_cert.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -e
 cd $(dirname $0)
 # Etape1.generer cle prive
 openssl genrsa -out server.key 2048
-
+cat > san.cnf
 #Etape2:generer CSR avec subject pour mqtt
 openssl req -new -key server.key -out server.csr
- -subj "/C=TN/ST=Tunis/L=Tunis/CN=mosquitto.server/0=IoT"
+ -subj "/C=TN/ST=Tunis/L=Tunis/CN=backend/0=IoT"
  -config openssl.cnf
- -addtext "subjectALtName=DNS:localhost,DNS:mosquitto,IP:127.0.0.1"
+ -addtext "subjectALtName=DNS:localhost,DNS:mosquitto,DNS:backend,DNS:mqtt,IP:127.0.0.1"
  -batch
 #Etape 3: signe avec CA 
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 365 -extensions server_ext -extfile openssl.cnf
