c221ad8c8083cdd962757bee4f4e9fe966e6de37
nadine-touil <nadine.std.touil@gmail.com>
2026-05-02 00:47:50 +0100

    Enhance create_device_cert.sh for device name input
    Updated script to accept device name as an argument and added error handling.

--- pki/create_device_cert.sh
+++ pki/create_device_cert.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-DEVICE_NAME=device
+set -e
+cd $(dirname $0)
 
-echo "generation cle prive..."
+DEVICE_NAME=${1:-device001}
+
+echo "[PKI] Generating private key for $DEVICE_NAME..."
 openssl genrsa -out ${DEVICE_NAME}.key 2048
 
-echo 
+echo "[PKI] Generating CSR..."
+openssl req -new -key ${DEVICE_NAME}.key -out ${DEVICE_NAME}.csr \
+    -subj "/C=TN/O=IoT_Project/CN=${DEVICE_NAME}"
+
+echo "[PKI] Signing with CA..."
+openssl x509 -req -in ${DEVICE_NAME}.csr -CA ca.crt -CAkey ca.key \
+    -CAcreateserial -out ${DEVICE_NAME}.crt -days 365 \
+    -extfile <(cat << EOF
+basicConstraints=CA:FALSE
+keyUsage=digitalSignature
+extendedKeyUsage=clientAuth
+subjectAltName=DNS:${DEVICE_NAME}
+EOF
+)
+
+echo "[PKI] Cleaning up..."
+rm -f ${DEVICE_NAME}.csr
+
+echo "[PKI] Done! Certificate: ${DEVICE_NAME}.crt"
