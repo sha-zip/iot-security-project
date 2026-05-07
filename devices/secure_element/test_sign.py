45611210312c5f9d42c51ff6ab925b8ea7c17f18
nadine-touil <nadine.std.touil@gmail.com>
2026-05-07 21:54:23 +0000

    nsalah f ghalta fel certs ama tsalhtch

--- devices/secure_element/test_sign.py
+++ devices/secure_element/test_sign.py
@@ -1,6 +1,7 @@
-from se_module import sign_data
-data = open('data.txt', 'rb').read()
-sig = sign_data(data)
-open('sig.bin', 'wb').write(sig)
-print('Signature sauvegardee dans sig.bin')
-print ('Taille signature:', len(sig), 'bytes')
+from se_module import SecureElement
+with SecureElement() as se:
+    data = open('data.txt', 'rb').read()
+    sig = se.sign(data)
+    open('sig.bin', 'wb').write(sig)
+    print ('Signature sauvegardee dans sig.bin')
+    print ('Taille signature:', len(sig), 'bytes')
